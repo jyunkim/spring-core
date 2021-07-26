4ff19be9a4d226edd117778e362caf4f227ab4f3
JyunKim <kann1997@naver.com>
2021-07-26 19:55:43 +0900

    test: Add member service test

---
+++ src/test/java/jihyun/core/member/MemberServiceTest.java
@@ -0,0 +1,22 @@
+package jihyun.core.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+
+    private final MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    void join() {
+        // given
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        // when
+        memberService.join(member);
+        Member result = memberService.findMember(1L);
+
+        // then
+        Assertions.assertThat(result).isEqualTo(member);
+    }
+}

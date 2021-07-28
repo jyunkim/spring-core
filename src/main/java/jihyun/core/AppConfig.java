285c56e91c9964ffc1f632b4b113ed7989dab472
JyunKim <kann1997@naver.com>
2021-07-28 16:51:19 +0900

    refactor: Remove duplicates by adding method

--- src/main/java/jihyun/core/AppConfig.java
+++ src/main/java/jihyun/core/AppConfig.java
@@ -1,6 +1,8 @@
 package jihyun.core;
 
+import jihyun.core.discount.DiscountPolicy;
 import jihyun.core.discount.FixedDiscountPolicy;
+import jihyun.core.member.MemberRepository;
 import jihyun.core.member.MemberService;
 import jihyun.core.member.MemberServiceImpl;
 import jihyun.core.member.MemoryMemberRepository;
@@ -10,10 +12,18 @@ import jihyun.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixedDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new FixedDiscountPolicy();
     }
 }

d103fea4d62c91d10259fe1e0816c6f640b76a8d
JyunKim <kann1997@naver.com>
2021-08-12 18:09:03 +0900

    Fix: Fix NoUniqueBeanDefinitionException

--- src/main/java/jihyun/core/discount/RateDiscountPolicy.java
+++ src/main/java/jihyun/core/discount/RateDiscountPolicy.java
@@ -2,7 +2,11 @@ package jihyun.core.discount;
 
 import jihyun.core.member.Grade;
 import jihyun.core.member.Member;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
 
+@Component
+@Primary
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10;

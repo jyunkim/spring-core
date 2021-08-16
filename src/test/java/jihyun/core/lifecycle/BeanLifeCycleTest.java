ffe44aba15bb6eda88cc55b5aa77cd567672fe40
JyunKim <kann1997@naver.com>
2021-08-16 19:15:53 +0900

    test: Add bean lifecycle test

---
+++ src/test/java/jihyun/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,30 @@
+package jihyun.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    void lifeCycleTest() {
+        // close()를 사용하기 위해 ConfigurableApplicationContext 사용
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient networkClient = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+
+//        @Bean(initMethod = "init", destroyMethod = "close")
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://www.spring.com");
+            return networkClient;
+        }
+    }
+}

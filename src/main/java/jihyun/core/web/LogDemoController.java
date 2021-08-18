ae596015d9b61ef3cd9ffdc16b1adf812bd87d08
JyunKim <kann1997@naver.com>
2021-08-18 20:06:01 +0900

    refactor: Change provider to proxy

--- src/main/java/jihyun/core/web/LogDemoController.java
+++ src/main/java/jihyun/core/web/LogDemoController.java
@@ -2,7 +2,6 @@ package jihyun.core.web;
 
 import jihyun.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,14 +13,13 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     // 자바 표준 서블릿 규약에 맞는 요청
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("Controller test");
